786deeb664fefc947d923ef34afd8facd8bd4717
knika-starkey <knika787@gmail.com>
2022-12-02 17:40:26 +0200

    ex + buttons

--- class.js
+++ class.js
@@ -23,14 +23,14 @@ window.onload = function () {
     onReady = () => {
       clearInterval(this.interval);
       clearTimeout(this.timer);
-      this.info.innerHTML += "Готово! ";
+      this.info.innerHTML = "Готово! ";
       this.state = "stopped";
       this.info.innerHTML += this.state;
     };
     stop() {
       clearInterval(this.interval);
       clearTimeout(this.timer);
-      this.info.innerHTML += "Примусове вимкнення!";
+      this.info.innerHTML = "Примусове вимкнення!";
       this.state = "stopped";
       this.info.innerHTML += this.state;
     }
@@ -38,84 +38,136 @@ window.onload = function () {
 
   class CoffeeMachine extends Machine {
     constructor(info) {
-      super();
+      super(info);
       this.drink = "вода";
     }
     run(drink) {
-      if (drink != undefined) {
-        this.drink = drink;
+      try {
+        if (this.state == "started") {
+          throw new Error("Машина занята!");
+        } else {
+          if (drink != undefined) {
+            this.drink = drink;
+          }
+          this.info.innerHTML = `Приготування: ${this.drink} `;
+          switch (this.drink) {
+            case "вода":
+              this.time = 2000;
+              break;
+            case "латте":
+              this.time = 5000;
+              break;
+            case "еспрессо":
+              this.time = 3000;
+              break;
+            case "матча латте":
+              this.time = 8000;
+              break;
+            case "горячий шоколад":
+              this.time = 6000;
+              break;
+            default:
+              this.info.innerHTML += "Такого напою немає!";
+              CofeeMachine.stop();
+              break;
+          }
+          super.run();
+        }
+      } catch (ex) {
+        this.info.innerHTML += ex.message;
       }
-      this.info.innerHTML = `Приготування: ${this.drink} `;
-      switch (this.drink) {
-        case "вода":
-          this.time = 2000;
-          break;
-        case "латте":
-          this.time = 5000;
-          break;
-        case "еспрессо":
-          this.time = 3000;
-          break;
-        case "матча латте":
-          this.time = 8000;
-          break;
-        case "горячий шоколад":
-          this.time = 6000;
-          break;
-        default:
-          this.info.innerHTML += "Такого напою немає!";
-          CofeeMachine.stop();
-          break;
-      }
-      super.run();
     }
   }
 
   class Multivariate extends Machine {
-    constructor() {
-      super();
+    constructor(info) {
+      super(info);
       this.cook = "кип'ятіння";
     }
     run(cook) {
-      if (cook != undefined) this.cook = cook;
-      this.info.innerHTML += "Приготування: " + this.cook + " ";
-      switch (this.cook) {
-        case "кип'ятіння":
-          this.time = 10000;
-          break;
-        case "суп":
-          this.time = 15000;
-          break;
-        case "тушкування":
-          this.time = 30000;
-          break;
-        case "випічки":
-          this.time = 17000;
-          break;
-        default:
-          this.info.innerHTML += "Такого немає!";
-          Multivariate.stop();
-          break;
+      try {
+        if (this.state == "started") {
+          throw new Error("Машина занята!");
+        } else {
+          if (cook != undefined) this.cook = cook;
+          this.info.innerHTML += "Приготування: " + this.cook + " ";
+          switch (this.cook) {
+            case "кип'ятіння":
+              this.time = 10000;
+              break;
+            case "суп":
+              this.time = 15000;
+              break;
+            case "тушкування":
+              this.time = 30000;
+              break;
+            case "випічки":
+              this.time = 17000;
+              break;
+            default:
+              this.info.innerHTML += "Такого немає!";
+              Multivariate.stop();
+              break;
+          }
+          super.run();
+        }
+      } catch (ex) {
+        this.info.innerHTML += ex.message;
       }
-      super.run();
     }
   }
 
-  let info = document.getElementById("textCoffeMachine");
+  let infoCoffee = document.getElementById("textCoffeMachine");
   let latte = document.getElementById("latte");
-  //   let espresso = document.getElementById("espresso");
-  //let stop = document.getElementById("stop");
+  let espresso = document.getElementById("espresso");
+  let matchaLatte = document.getElementById("matchaLatte");
+  let hotChocolate = document.getElementById("hotChocolate");
+  let stopCoffe = document.getElementById("stopCoffe");
 
   // let machine = new Machine(info);
-  let coffeeMachine = new CoffeeMachine(info);
+  let coffeeMachine = new CoffeeMachine(infoCoffee);
 
   latte.addEventListener("click", function () {
     coffeeMachine.run("латте");
   });
 
-  //   espresso.addEventListener("click", function () {
-  //     coffeeMachine.run("espresso");
-  //   });
+  espresso.addEventListener("click", function () {
+    coffeeMachine.run("еспрессо");
+  });
+  matchaLatte.addEventListener("click", function () {
+    coffeeMachine.run("матча латте");
+  });
+
+  hotChocolate.addEventListener("click", function () {
+    coffeeMachine.run("горячий шоколад");
+  });
+
+  stopCoffe.addEventListener("click", function () {
+    coffeeMachine.stop();
+  });
+
+  let infoMulti = document.getElementById("textMultivariate");
+  let soup = document.getElementById("soup");
+  let stewing = document.getElementById("stewing");
+  let baking = document.getElementById("baking");
+  let stopMulti = document.getElementById("stopMulti");
+
+  let multivariate = new Multivariate(infoMulti);
+
+  soup.addEventListener("click", function () {
+    multivariate.run("soup");
+  });
+
+  stewing.addEventListener("click", function () {
+    Multivariate.run("еспрессо");
+  });
+  baking.addEventListener("click", function () {
+    multivariate.run("матча латте");
+  });
+
+  stopMulti.addEventListener("click", function () {
+    multivariate.stop();
+  });
   // let multivariate = new Multivariate();
   // multivariate.run(prompt("Що сьогодні готуємо?"));
   //   let cofeeMachine = new CoffeeMachine(info);
